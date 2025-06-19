a28fb2bf2caee5b1fb4b7ee3a257e9e320112415
Yanshuf <sefiuzan812@gmail.com>
2025-06-19 09:51:12 +0300

    log nod env

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -12,6 +12,7 @@ export const client = createClient<AppRouter>({
 function getBaseUrl() {
   // In production, use the production worker
   if (process.env.NODE_ENV === "production") {
+    console.log("production")
     return process.env.CF_WORKERS_URL
   }
  
